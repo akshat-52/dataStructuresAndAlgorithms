8e727a14e0c5b6545c80d0c10f094ffa1076fab0
akshat-52 <52.akkey@gmail.com>
2021-09-30 20:53:33 +0530

    perform the bubble sort

--- bubbleSortAlgo.c
+++ bubbleSortAlgo.c
@@ -9,3 +9,26 @@ end bubbleSort
 */
 
 // bubbleSortAlgo Code
+#include <stdio.h>
+
+//perform the bubble sort
+void bubbleSort(int array[], int size){
+
+    //loop to access each array element
+    for(int step=0; step<size-1; ++step){
+
+        //loop to compare array elements
+        for(int i=0; i<size-step-1; ++i){
+
+            //compare two adjacent elements
+            //change > to < to sort in descending order
+            if(array[i] > array[i+1]){
+
+                //swapping occurs if elements are not in intended order
+                int temp=array[i];
+                array[i]=array[i+1];
+                array[i+1]=temp;
+            }
+        }
+    }
+}
